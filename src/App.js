a468cefc77e00531347107e7517df4100c8fe5cd
Malcolm McConaghy <mcconaghym@live.co.uk>
2019-11-13 18:48:50 +0000

    Update logo svg. Better styling for cards and structure of components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Card from './components/Card';
 import Header from './components/Header';
 import SelectDocuments from './components/SelectDocuments';
-import './styles/App.css';
+import './styles/App.scss';
 
 function App() {
   return (
@@ -11,21 +11,23 @@ function App() {
       <main>
         <h1>Right to Rent Check</h1>
         <p>What documents do we accept? To keep this process smooth make sure that you have these documents to hand, with these it won't take any longer than 30 seconds</p>
-        <Card
-          title="Select 2 documents from the list:"
-          buttonText="Proceed with selected docs"
-        >
-          <SelectDocuments />
-        </Card>
-        <Card
-          title="Select 2 documents from the list:"
-          buttonText="Proceed with selected docs"
-          secondaryButton
-        >
-          <p>
-            You only need to upload one page with the photo from your passport. Additionally, we will ask for visa for non-european passports.
-          </p>
-        </Card>
+        <div className="cards">
+          <Card
+            title="Select 2 documents from the list:"
+            buttonText="Proceed with selected docs"
+          >
+            <SelectDocuments />
+          </Card>
+          <Card
+            title="Select 2 documents from the list:"
+            buttonText="Proceed with selected docs"
+            secondaryButton
+          >
+            <p>
+              You only need to upload one page with the photo from your passport. Additionally, we will ask for visa for non-european passports.
+            </p>
+          </Card>
+        </div>
       </main>
     </div>
   );
